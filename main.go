673fc5e0bce7191345d8382be8d831a807588533
André Freitas <freitasandre38@gmail.com>
2023-07-27 18:35:21 -0300

    feat: create makefile

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+)
+
 func main() {
 	service := NewCatFactService("https://catfact.ninja/fact")
 	service = NewLoggerService(service)
+
+	fact, err := service.GetCatFact(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", fact)
 }
